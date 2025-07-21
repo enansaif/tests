ef5e5ca8fc262f0b361ce0e8891c15e3a96b2943
enansaif <enansaifme33@gmail.com>
2025-07-21 12:15:11 +0600

    working version

--- Vendor/CheckoutCustomField/view/frontend/requirejs-config.js
+++ Vendor/CheckoutCustomField/view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     config: {
         mixins: {
-            'Magento_Checkout/js/view/shipping-address/address-renderer/default': {
+            'Magento_Checkout/js/view/shipping': {
                 'Vendor_CheckoutCustomField/js/view/checkout/shipping/custom-field-mixin': true
             }
         }
--- Vendor/CheckoutCustomField/view/frontend/web/js/view/checkout/shipping/custom-field-mixin.js
+++ Vendor/CheckoutCustomField/view/frontend/web/js/view/checkout/shipping/custom-field-mixin.js
@@ -1,32 +1,37 @@
 define([
-    'Magento_Ui/js/form/element/abstract',
     'uiLayout',
-    'Magento_Checkout/js/model/quote'
-], function (Abstract, layout, quote) {
+    'Magento_Checkout/js/model/quote',
+    'Magento_Ui/js/form/form'
+], function (layout, quote) {
     'use strict';
 
-    return function (Component) {
-        return Component.extend({
+    return function (Shipping) {
+        return Shipping.extend({
             initialize: function () {
-                console.log('Custom field mixin loaded');
                 this._super();
 
-                this.customField = ko.observable('');
+                console.log('✅ Custom field mixin loaded');
 
                 layout([
                     {
                         parent: 'checkout.steps.shipping-step.shippingAddress.shipping-address-fieldset',
-                        component: 'Magento_Ui/js/form/element/abstract',
                         name: 'custom_field',
+                        component: 'Magento_Ui/js/form/element/abstract',
+                        config: {
+                            customScope: 'shippingAddress',
+                            template: 'ui/form/field',
+                            elementTmpl: 'ui/form/element/input',
+                            id: 'custom-field'
+                        },
                         dataScope: 'shippingAddress.custom_field',
                         label: 'Custom Field',
                         provider: 'checkoutProvider',
-                        sortOrder: 250,
                         visible: true,
                         validation: {
                             'required-entry': false
                         },
-                        value: ''
+                        sortOrder: 250,
+                        id: 'custom-field'
                     }
                 ]);
 
